d1d175f7a0385c3938d76221f020395e2cfe6eaa
SSNgwenya <sunnyngwenya@gmail.com>
2024-03-08 03:44:49 -0800

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,13 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if an argument is provided
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <input_string>"
-  exit 1
-end
-
-# Apply the regular expression to the input argument
-matches = ARGV[0].scan(/School/).join
-
-# Output the matched result
-puts matches
+puts ARGV[0].scan(/School/).join
