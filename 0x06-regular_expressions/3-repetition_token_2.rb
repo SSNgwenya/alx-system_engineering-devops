c89c02adabc45d84ff5504a03a3e0e6b51ec6b48
SSNgwenya <sunnyngwenya@gmail.com>
2024-03-08 03:30:57 -0800

    3-repetition_token_2.rb

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,2 +1,13 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/^h.n$/).join
+
+# Check if an argument is provided
+if ARGV.empty?
+  puts "Usage: #{$PROGRAM_NAME} <input_string>"
+  exit 1
+end
+
+# Apply the corrected regular expression to the input argument
+matches = ARGV[0].scan(/hbt*n/).join
+
+# Output the matched result
+puts matches
