5855965ce2b1499b734df35dfabe7a3eabc1af92
SSNgwenya <sunnyngwenya@gmail.com>
2024-05-31 08:17:55 -0700

    0x0A. Configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
+# Creates a file
+
 file { '/tmp/school':
-  ensure  => file,
-  content => 'I love Puppet',
-  mode    => '07744',
-  owner   => 'www-data',
-  group   => 'www-data',
+	mode	=> '0744',
+	owner	=> 'www-data',
+	group	=> 'www-data',
+	content	=> 'I love Puppet',
 }
+
