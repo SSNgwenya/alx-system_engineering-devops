8f89851c6f0f8031353a2866089389fe6651d52c
SSNgwenya <sunnyngwenya@gmail.com>
2024-06-26 05:36:51 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # Installing a specified version of flask (2.1.0)
-
-package {'flask':
-	ensure	=> '2.1.0',
-	provider => 'pip'
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip',
 }
