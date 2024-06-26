bee47155797bedaaa981215e898f9066dfddbf7e
SSNgwenya <sunnyngwenya@gmail.com>
2024-06-26 07:39:13 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
-# Installing a specified version of flask (2.1.0)
+# Installing version 2.1.0 of flask using puppet
+
+package { 'Python3.8':
+    ensure   => '3.8.10',
+    provider => 'pip3',
+}
+
 package { 'flask':
     ensure   => '2.1.0',
-    provider => 'pip',
+    provider => 'pip3',
+}
+
+package { 'Werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3',
+    require  => Package['Flask'],
 }
